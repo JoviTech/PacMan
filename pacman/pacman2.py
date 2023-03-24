ae5c1fa7e6dfb5d8f5a3cc65f81ff57710e0a2ea
JoviTech <jovitechgaropaba@gmail.com>
2023-03-24 18:52:49 -0300

    implementação de regras

--- pacman/pacman2.py
+++ pacman/pacman2.py
@@ -2,7 +2,10 @@ import pygame
 
 pygame.init()
 
-SCREEN = pygame.display.set_mode((800, 600), 0)
+
+LARGURA_TELA = 800
+ALTURA_TELA = 600
+TELA = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA), 0)
 
 AMARELO = (255, 255, 0)
 PRETO = (0, 0, 0)
@@ -11,8 +14,27 @@ class PacMan:
     def __init__(self):
         self.centro_x = 400
         self.centro_y = 300
-        self.tamanho = 100
+        self.vel_x = 1
+        self.vel_y = 1
+        self.tamanho = LARGURA_TELA // 30 #largura / int(numero de colunas do jogo)
         self.raio = self.tamanho//2 # "//" arredonda para inteiro
+        self.coluna = 0
+        self.linha = 0
+
+    def calcula_regras(self):
+        self.coluna += self.vel_x
+        self.linha += self.vel_y
+        self.centro_x  = int(self.coluna * self.tamanho + self.raio)
+        self.centro_y = int(self.linha * self.tamanho + self.raio)
+
+        if self.centro_x + self.raio > LARGURA_TELA:
+            self.vel_x = - 1
+        elif self.centro_x - self.raio < 0:
+            self.vel_x = 1
+        elif self.centro_y + self.raio > ALTURA_TELA:
+            self.vel_y = -1
+        elif self.centro_y - self.raio < 0:
+            self.vel_y = 1
 
     def pintar(self, tela):
         #Desenha o corpo do pacman
@@ -35,10 +57,16 @@ if __name__ == "__main__":
     pacman = PacMan()
 
     while True:
-        #Pintar a tela
-        pacman.pintar(SCREEN)
-        pygame.display.update()
+        #Calcula regras
+        pacman.calcula_regras()
 
+        #Pintar a tela
+        TELA.fill(PRETO)
+        pacman.pintar(TELA)
+        pygame.display.update()
+        pygame.time.delay(100)
+
+        #Captura eventos
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 exit()
